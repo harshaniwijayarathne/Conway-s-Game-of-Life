cb40ac4a3b9caf47f09b9ce046d2683e7c55551b
Harshani <harshani.wijaya@gmail.com>
2026-09-21 11:15:35 +0530

    initialize main.py with pygame import

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+
+pygame.init()
+
+WINDOW_WIDTH = 750
+WINDOW_HEIGHT = 750
+
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Game of Life") 
+
