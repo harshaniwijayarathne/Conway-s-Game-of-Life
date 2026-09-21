a2f9350c00c35bd9d77fc97dede255f0af261c87
Harshani <harshani.wijaya@gmail.com>
2026-09-21 12:39:20 +0530

    implement Grid class and render cells in main simulation loop

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import pygame, sys 
+from grid import Grid
 
 pygame.init()
 
 GREY = (29,29,29)
 WINDOW_WIDTH = 750
 WINDOW_HEIGHT = 750
+CELL_SIZE = 25
 FPS = 12
 
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Game of Life") 
 
 clock = pygame.time.Clock()
+
+grid = Grid(WINDOW_WIDTH, WINDOW_HEIGHT, CELL_SIZE)
 
 #simulation loop
 while True:
@@ -25,6 +29,7 @@ while True:
 
     # 3. Drawing
     window.fill(GREY)
+    grid.draw(window)
 
     pygame.display.update()
     clock.tick(FPS)
